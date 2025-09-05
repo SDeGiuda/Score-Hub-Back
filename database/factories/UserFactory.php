a17a2f9ac49209486316d32ddb62040dca4453b1
SDeGiuda <santiagodegiuda@gmail.com>
2025-09-05 09:12:49 -0300

    feat: add migration and factory for users

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -31,9 +31,7 @@ class UserFactory extends Factory
         return [
             'name' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
-            'email_verified_at' => now(),
-            'password' => static::$password ??= Hash::make('>e$pV4chNFcJoAB%X#{'),
-            'remember_token' => Str::random(10),
+            'password' => static::$password ??= Hash::make('password'),
         ];
     }
 
