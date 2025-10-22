e0029f45534987dee7127a6827bf4fbe3146e23c
phillipbowles <pbowles@correo.um.edu.uy>
2025-10-22 08:32:21 -0300

    hooks changes

--- src/Games/Domain/Models/Game.php
+++ src/Games/Domain/Models/Game.php
@@ -12,6 +12,7 @@ use Src\Matches\Domain\Models\GameMatch;
  * @method static \Illuminate\Database\Eloquent\Builder<static>|Game newModelQuery()
  * @method static \Illuminate\Database\Eloquent\Builder<static>|Game newQuery()
  * @method static \Illuminate\Database\Eloquent\Builder<static>|Game query()
+ *
  * @property int                          $id
  * @property string                       $name
  * @property int                          $number_of_players
@@ -23,6 +24,7 @@ use Src\Matches\Domain\Models\GameMatch;
  * @property string|null                  $rules
  * @property \Carbon\CarbonImmutable|null $created_at
  * @property \Carbon\CarbonImmutable|null $updated_at
+ *
  * @method static \Illuminate\Database\Eloquent\Builder<static>|Game whereCreatedAt($value)
  * @method static \Illuminate\Database\Eloquent\Builder<static>|Game whereHasTeams($value)
  * @method static \Illuminate\Database\Eloquent\Builder<static>|Game whereHasTurns($value)
@@ -34,30 +36,37 @@ use Src\Matches\Domain\Models\GameMatch;
  * @method static \Illuminate\Database\Eloquent\Builder<static>|Game whereTeamLength($value)
  * @method static \Illuminate\Database\Eloquent\Builder<static>|Game whereTurnDuration($value)
  * @method static \Illuminate\Database\Eloquent\Builder<static>|Game whereUpdatedAt($value)
+ *
  * @property-read \Illuminate\Database\Eloquent\Collection<int, GameMatch> $matches
  * @property-read int|null $matches_count
  * @property int    $rounds
  * @property string $ending
  * @property int    $min_team_length
  * @property int    $max_team_length
+ *
  * @method static \Illuminate\Database\Eloquent\Builder<static>|Game whereEnding($value)
  * @method static \Illuminate\Database\Eloquent\Builder<static>|Game whereMaxTeamLength($value)
  * @method static \Illuminate\Database\Eloquent\Builder<static>|Game whereMinTeamLength($value)
  * @method static \Illuminate\Database\Eloquent\Builder<static>|Game whereRounds($value)
+ *
  * @property int $min_points
  * @property int $max_points
  * @property int $user_id
+ *
  * @method static \Illuminate\Database\Eloquent\Builder<static>|Game whereMaxPoints($value)
  * @method static \Illuminate\Database\Eloquent\Builder<static>|Game whereMinPoints($value)
  * @method static \Illuminate\Database\Eloquent\Builder<static>|Game whereUserId($value)
+ *
  * @property string|null $description
- * @property string $icon
- * @property string $color
- * @property string $bg_color
+ * @property string      $icon
+ * @property string      $color
+ * @property string      $bg_color
+ *
  * @method static \Illuminate\Database\Eloquent\Builder<static>|Game whereBgColor($value)
  * @method static \Illuminate\Database\Eloquent\Builder<static>|Game whereColor($value)
  * @method static \Illuminate\Database\Eloquent\Builder<static>|Game whereDescription($value)
  * @method static \Illuminate\Database\Eloquent\Builder<static>|Game whereIcon($value)
+ *
  * @mixin \Eloquent
  */
 class Game extends Model
