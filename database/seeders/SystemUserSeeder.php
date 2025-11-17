4997cb95ad07df3eff867f5370bc496ba14d5283
SDeGiuda <santiagodegiuda@gmail.com>
2025-11-16 22:44:50 -0300

    seeder prod friendly

--- database/seeders/SystemUserSeeder.php
+++ database/seeders/SystemUserSeeder.php
@@ -9,12 +9,18 @@ use Database\Factories\UserFactory;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
+use Src\Users\Domain\Models\User;
 
 class SystemUserSeeder extends Seeder
 {
     public function run(): void
     {
         // Insert system user with ID 0 for classic games
-        UserFactory::new()->createOne(['id' => 0]);
+        $user = new User(['id'=>0]);
+        $user->name = 'admin';
+        $user->username = 'admin';
+        $user->password = 'password';
+        $user->email = 'fake@email.com';
+        $user->save();
     }
 }
